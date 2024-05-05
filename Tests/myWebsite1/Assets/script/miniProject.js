901f1842e194651ac2db9723c75376018514d2d2
wonderful-coyote <89476730+wonderful-coyote@users.noreply.github.com>
2024-05-05 14:21:06 +0100

    Add files via upload
    these are some of my newbie projects courtesy of EDSA

---
+++ Tests/myWebsite1/Assets/script/miniProject.js
@@ -0,0 +1,11 @@
+/*Use JavaScript to create an event listener for the submit button.
+
+In the event listener, get the value of the text input field and perform the following operations on the string:
+Count the number of characters in the string.
+Count the number of words in the string.
+Reverse the string
+Convert the string to uppercase.
+Convert the string to lowercase.
+
+Display the results of the operations on the string in the HTML page, below the form*/
+
